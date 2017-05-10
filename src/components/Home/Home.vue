b8f406c86a6838575bb1cac74c7b74f7c9776868
dhq <863086441@qq.com>
2017-05-10 21:23:25 +0800

    vueProject

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -3,7 +3,7 @@
 		<!-- 利用mint-ui中的swipe组件实现轮播图 -->
 		<mt-swipe :auto="4000">
 		  <mt-swipe-item v-for="(item,index) in lists" :key="index">
-		  	<img :src="item.img" alt="">
+		  	<a :href="item.url"><img :src="item.img" alt=""></a>
 		  </mt-swipe-item>
 		</mt-swipe>
 
@@ -55,35 +55,48 @@
 export default{
 	data(){
 		return {
-			lists:[
-				{img:'https://pic.hiweixiu.com/images/uploadImg/banner/201705/20170508171912_320.png'},
-				{img:'https://pic.hiweixiu.com/images/uploadImg/banner/201705/20170508171925_490.png'},
-				{img:'https://pic.hiweixiu.com/images/uploadImg/banner/201705/20170508171856_106.png'}
-			]
+			lists:[]
 		}
 	},
 	created(){
+		this.getImage()
 	},
 	methods:{
+		getImage(){
+			var url='http://localhost:1666/api/getlunbo'
+			this.$http.get(url).then(res=>{
+				this.lists=res.body.message
+			},res=>{
+				alert('发生异常')
+			})
+		}
 	}
 }
 </script>
 
 <style scoped>  /*这里的scoped属性是保证里面的样式只对当前组件生效*/
-	.mint-swipe{
-		height: 155px;
-	}
 	img{
 		width: 100%;
 		height: 100%;
 	}
-	.mui-content > .mui-table-view:first-child {
-	    margin-top: 0px;
+	.mint-swipe{
+		height: 155px;
 	}
 	.icon-home{
 		width: 40px;
 		height: 40px;
-		background:url('../../../static/images/nav.png') no-repeat;
-		background-size:  40px 40px;
+		background:url('../../../static/images/nav.png') round;
+	}
+	.mui-grid-view.mui-grid-9{
+		background-color:#fff;
+		border-left: none;
+		border-top: none;
+	}
+	.mui-grid-view.mui-grid-9 .mui-table-view-cell{
+		border-right: none;
+		border-bottom:none;
+	}
+	.mui-content > .mui-table-view:first-child {
+	    margin-top: 0px;
 	}
 </style>
