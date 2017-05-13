c7ba06ee3b3b09ee8381f158d991c552aeac1b42
dhq <863086441@qq.com>
2017-05-13 22:13:16 +0800

    vueProject

--- src/components/common/comment.vue
+++ src/components/common/comment.vue
@@ -46,7 +46,7 @@
 					this.$toast('评论内容必须填写')
 					return
 				}
-				var url='http://localhost:1666/api/postcomment/'+this.artid
+				var url='http://webhm.top:8899/api/postcomment/'+this.artid
 				this.$http.post(url,{content:contentText},{emulateJSON:true}).then(res=>{
 					this.$toast('评论提交成功!')
 					this.getcomment(this.pageindex)
@@ -57,7 +57,7 @@
 			},
 			// 获取评论
 			getcomment(pageindex){
-				var url='http://localhost:1666/api/getcomments/'+this.artid+'?pageindex='+pageindex
+				var url='http://webhm.top:8899/api/getcomments/'+this.artid+'?pageindex='+pageindex
 				this.$http.get(url).then(res=>{
 					// this.comments=res.body.message
 					this.comments=this.comments.concat(res.body.message)
